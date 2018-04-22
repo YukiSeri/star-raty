9819b3e356869fd4d122ff181232c610be282726
Seri-Lis <seri@lifeis.style>
2018-04-22 19:02:23 +0900

    feat:set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-
-  get 'reviews/show'
-
-  get 'reviews/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'reviews#index'
+  resources :reviews, only: [:index, :new, :create, :show]
 end
