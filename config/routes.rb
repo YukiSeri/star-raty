dba8c35a0219080c6b6a87323453ee410ddea218
Seri-Lis <seri@lifeis.style>
2018-04-22 18:39:21 +0900

    feat:rails g controller Reviews new show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/show'
+
+  get 'reviews/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
