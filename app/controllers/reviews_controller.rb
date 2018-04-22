df2159576c24869a402f2bffd937b795f913e9e3
Seri-Lis <seri@lifeis.style>
2018-04-22 20:44:13 +0900

    feat:create create_action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,19 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def create
+    Review.create(create_params)
+    render :index
+  end
+
   def show
   end
 
   def index
   end
+end
+
+private
+def create_params
+  params.require(:review).permit(:title, :star, :memo)
 end
